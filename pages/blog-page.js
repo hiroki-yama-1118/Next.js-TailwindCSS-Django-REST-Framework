1d758ba56ded1261eb2390ac2f702772ecc49db6
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-04-03 17:36:53 +0900

    ビルド時にサーバーサイドで実行する関数作成

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -4,3 +4,10 @@ const Blog = () => {
   return <Layout title="Blog">Blog Page</Layout>;
 };
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
