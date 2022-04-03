5c62f5acb5350127c44679fc5839f158caf4d36c
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-04-03 16:15:23 +0900

    フッター追加

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Layout({ children, title = "HP by Nextjs" }) {
   return (
@@ -35,6 +36,17 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
         {/* childrenとして渡ってきた全ての要素を表示 */}
         {children}
       </main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </a>
+      </footer>
     </div>
   );
 }
